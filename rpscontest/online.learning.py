e12426c865da3431e7a11e199a501305b1d5d81f
Anderson Mesquita <andersonvom@gmail.com>
2011-12-17 10:37:56 -0500

    Removed loop to simulate matches

--- rpscontest/online.learning.py
+++ rpscontest/online.learning.py
@@ -26,36 +26,34 @@ def updateTheta(theta, x, y, alpha):
 	# return theta
 
 
-input = ''
 matches = 10
-for i in range(matches):
-	if input == "":
-		choices = ['R','P','S']
-		temp_list = [0] * matches
-		X = list(temp_list)
-		Y = list(temp_list)
-		Theta = [ list(temp_list), list(temp_list), list(temp_list) ]
-		output = random.choice(choices)
-		X[-1] = choices.index(output) + 1
-		alpha = 0.1
-	else:
-		Y.pop(0)
-		Y.append(choices.index(input) + 1)
-		for c in [1,2,3]:
-			Y_i = map(lambda i: 1 if (i==c) else 0, Y)
-			Theta[c-1] = updateTheta(Theta[c-1], X, Y_i, alpha)
-		#print Theta
-		pred = list(X)
-		pred.pop(0)
-		pred.append(0)
-		probabilities = [0,0,0]
-		for c in [1,2,3]:
-			pred[-1] = c # choices[c-1]
-			probabilities[c-1] = z(Theta[c-1], pred)
-		print probabilities
-		best_choice = probabilities.index(max(probabilities))
-		X.pop(0)
-		X.append(best_choice)
-		output = choices[best_choice]
-	print output
-	input = random.choice('RPS')
+if input == "":
+	choices = ['R','P','S']
+	temp_list = [0] * matches
+	X = list(temp_list)
+	Y = list(temp_list)
+	Theta = [ list(temp_list), list(temp_list), list(temp_list) ]
+	output = random.choice(choices)
+	X[-1] = choices.index(output) + 1
+	alpha = 0.1
+else:
+	Y.pop(0)
+	Y.append(choices.index(input) + 1)
+	for c in [1,2,3]:
+		Y_i = map(lambda i: 1 if (i==c) else 0, Y)
+		Theta[c-1] = updateTheta(Theta[c-1], X, Y_i, alpha)
+	#print Theta
+	pred = list(X)
+	pred.pop(0)
+	pred.append(0)
+	probabilities = [0,0,0]
+	for c in [1,2,3]:
+		pred[-1] = c # choices[c-1]
+		probabilities[c-1] = z(Theta[c-1], pred)
+	print probabilities
+	best_choice = probabilities.index(max(probabilities))
+	X.pop(0)
+	X.append(best_choice)
+	output = choices[best_choice]
+print output
+input = random.choice('RPS')
