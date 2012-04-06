7a6a7f448317c3be9111825bd52b4c0ae0d33347
Anderson Mesquita <andersonvom@gmail.com>
2012-04-06 19:30:13 -0300

    Finished solving easy puzzles completely and added display animation

--- sudoku_solver/sudoku.rb
+++ sudoku_solver/sudoku.rb
@@ -21,11 +21,10 @@ class Sudoku
     self.squares = self.unit_list.flatten.uniq.sort
     self.units   = Hash.new []
     self.peers   = Hash.new []
+    self.values  = Hash.new
 
     self.squares.each do |square|
       self.values[square] = DIGITS.join
-      #self.units[square] ||= []
-      #self.peers[square] ||= []
       self.unit_list.each do |unit|
         if unit.include? square
           self.units[square] += unit
@@ -33,19 +32,19 @@ class Sudoku
         end
       end
     end
-
-    parse
   end
 
   def parse
-    grid = Hash[self.squares.zip self.raw_entry.chars]
+    display
+    grid = Hash[self.squares.zip self.raw_entry.chars.to_a]
     grid.each do |key, value|
       assign(key, value) if value =~ /[1-9]/
     end
+    nil
   end
 
   def assign(square, value)
-    other_values = self.values[key].gsub value, ''
+    other_values = self.values[square].gsub value, ''
 
     contradiction = false
     other_values.chars { |other_value| contradiction |= !eliminate(square, other_value) }
@@ -56,6 +55,7 @@ class Sudoku
     return true unless self.values[square].include? value # already eliminated
 
     self.values[square].gsub! value, '' # eliminate from given square
+    display
 
     case self.values[square].size
     # If no other possible values exist, then its a contradiction!
@@ -64,26 +64,36 @@ class Sudoku
     # When only one possible value exists, then remove this value from this square's peers
     when 1
       contradiction = false
-      self.peers[square].each { |peer| contradiction |= !eliminate peer, self.values[square] }
+      self.peers[square].each { |peer| contradiction |= !eliminate(peer, self.values[square]) }
       return false if contradiction
     end
 
-    # TODO: use another strategy
+    # Verify possible places for 'value' after removing it from 'square'
+    possible_places = self.units[square].map { |unit| unit if self.values[unit].include? value }.compact
+    case possible_places.size
+    when 0
+      return false # contradiction since there are no more places to put 'value'
+    when 1
+      return false unless assign(possible_places.first, value)
+    end
 
     true
   end
 
   def display
+    sleep(0.01)
+    system "clear"
     line = [ ['-'*(WIDTH*3+1)] * 3 ].join '+'
     ROWS.each do |r|
       row = []
       COLS.each do |c|
-        row << '123412344' # self.values[r+c] || '123456789'
+        row << (self.values[r+c] || '123456789').center(DIGITS.size)
         row << '|' if c =~ /[36]/
       end
       puts ' ' + row.join(' ')
       puts line if r =~ /[CF]/
     end
+    nil
   end
 
 end
