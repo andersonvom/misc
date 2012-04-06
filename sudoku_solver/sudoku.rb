f7aa4a4b6f39f190e62e6aba641d8b7c7836b70e
Anderson Mesquita <andersonvom@gmail.com>
2012-04-06 02:34:07 -0400

    Started implementation of a parse and insert nem numbers method

--- sudoku_solver/sudoku.rb
+++ sudoku_solver/sudoku.rb
@@ -3,8 +3,9 @@ class Sudoku
   attr_accessor :raw_entry, :squares, :values, :unit_list, :units, :peers
 
   WIDTH = 10
+  DIGITS = %w( 1 2 3 4 5 6 7 8 9 )
   ROWS = %w( A B C D E F G H I )
-  COLS = %w( 1 2 3 4 5 6 7 8 9 )
+  COLS = DIGITS.clone
 
   def initialize(raw_entry)
     self.raw_entry = raw_entry
@@ -17,14 +18,14 @@ class Sudoku
       end
     end
 
-    self.squares = self.unit_list.flatten.uniq
-    self.units   = {}
-    self.peers   = {}
-    self.values  = {}
+    self.squares = self.unit_list.flatten.uniq.sort
+    self.units   = Hash.new []
+    self.peers   = Hash.new []
 
     self.squares.each do |square|
-      self.units[square] ||= []
-      self.peers[square] ||= []
+      self.values[square] = DIGITS.join
+      #self.units[square] ||= []
+      #self.peers[square] ||= []
       self.unit_list.each do |unit|
         if unit.include? square
           self.units[square] += unit
@@ -32,6 +33,44 @@ class Sudoku
         end
       end
     end
+
+    parse
+  end
+
+  def parse
+    grid = Hash[self.squares.zip self.raw_entry.chars]
+    grid.each do |key, value|
+      assign(key, value) if value =~ /[1-9]/
+    end
+  end
+
+  def assign(square, value)
+    other_values = self.values[key].gsub value, ''
+
+    contradiction = false
+    other_values.chars { |other_value| contradiction |= !eliminate(square, other_value) }
+    !contradiction
+  end
+
+  def eliminate(square, value)
+    return true unless self.values[square].include? value # already eliminated
+
+    self.values[square].gsub! value, '' # eliminate from given square
+
+    case self.values[square].size
+    # If no other possible values exist, then its a contradiction!
+    when 0
+      return false
+    # When only one possible value exists, then remove this value from this square's peers
+    when 1
+      contradiction = false
+      self.peers[square].each { |peer| contradiction |= !eliminate peer, self.values[square] }
+      return false if contradiction
+    end
+
+    # TODO: use another strategy
+
+    true
   end
 
   def display
